08b963809426e47958afd88639f8ebd7b7b8494f
Kalomiris Nikos <nikalomiris@gmail.com>
2018-09-13 17:05:23 +0300

    Corrects typo in console message.

--- src/com/kalomiris/Main.java
+++ src/com/kalomiris/Main.java
@@ -16,12 +16,12 @@ public class Main {
         int[] nextMove = {0, 0, 0, 0};
 
         while (!Board.isGameOver()) {
-            System.out.print((turn ? "Whites turn " : "Blacks turn ") + "Enter your next move (fromX fromY toX toY, e.g. 0 0 1 2): ");
+            System.out.print((turn ? "White\'s turn " : "Blacks turn ") + "please enter your next move (fromX fromY toX toY, e.g. 0 0 1 2): ");
             for (int i = 0; i < 4; i++) {
                 nextMove[i] = input.nextInt();
             }
             System.out.print("\n");
-            System.out.println("Next move for " + (turn ? "Whites " : "Blacks ") + "is " + Board.getPiecePositions()[nextMove[0]][nextMove[1]].getName()
+            System.out.println("Next move for " + (turn ? "White " : "Black ") + "is " + Board.getPiecePositions()[nextMove[0]][nextMove[1]].getName()
                 + " from position (" + nextMove[0] + ", " + nextMove[1] + ") to position (" + nextMove[2] + ", " + nextMove[3] + ")\n\n");
             Board.changePiecePosition(Board.getPiecePositions()[nextMove[0]][nextMove[1]], nextMove);
             Board.printBoard();
