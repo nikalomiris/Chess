5406fde8386edfcf162b23305612ea786147b1b8
Kalomiris Nikos <nikalomiris@gmail.com>
2018-09-17 14:40:50 +0300

    Adds unitTests for Bishop.

--- src/com/kalomiris/model/pieces/Rook.java
+++ src/com/kalomiris/model/pieces/Rook.java
@@ -29,7 +29,7 @@ public class Rook extends Piece {
         int[][] result = new int[numberOfSteps + 1][2];
         result[0][0] = m_XPosition;
         result[0][1] = m_YPosition;
-        for (int i = 1; i <= numberOfSteps; i++) { // TODO check that.
+        for (int i = 1; i <= numberOfSteps; i++) {
             result[i][0] = upRightXY(finalx, finaly)[1] ? m_XPosition + i
                     : (upRightXY(finalx, finaly)[2] ? m_XPosition : m_XPosition - i);
             result[i][1] = upRightXY(finalx, finaly)[0] ? m_YPosition + i
---
+++ test/com/kalomiris/model/pieces/BishopTest.java
@@ -0,0 +1,34 @@
+package com.kalomiris.model.pieces;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BishopTest {
+    Bishop bishop = new Bishop(3, 4, "White");
+    @Test
+    public void isValidPath() throws Exception {
+        assertFalse(bishop.isValidPath(3, 6));
+        assertFalse(bishop.isValidPath(3, 3));
+        assertFalse(bishop.isValidPath(7, 4));
+        assertFalse(bishop.isValidPath(2, 4));
+
+        assertTrue(bishop.isValidPath(6, 7));
+        assertTrue(bishop.isValidPath(1, 2));
+        assertTrue(bishop.isValidPath(6, 1));
+        assertTrue(bishop.isValidPath(1, 6));
+    }
+
+    @Test
+    public void drawPath() throws Exception {
+        int[][] path = {{3,4},{4,5},{5,6},{6,7}};
+        int[][] path1 = {{3,4},{4,3},{5,2},{6,1},{7,0}};
+        int[][] path2 = {{3,4},{2,5},{1,6},{0,7}};
+        int[][] path3 = {{3,4},{2,3},{1,2},{0,1}};
+        assertArrayEquals(path, bishop.drawPath(6, 7));
+        assertArrayEquals(path1, bishop.drawPath(7, 0));
+        assertArrayEquals(path2, bishop.drawPath(0, 7));
+        assertArrayEquals(path3, bishop.drawPath(0, 1));
+    }
+
+}
