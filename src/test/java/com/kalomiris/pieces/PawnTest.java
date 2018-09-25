6e0279555a5efe149e98943ef2a86dfdf173fc47
Kalomiris Nikos <nikalomiris@gmail.com>
2018-09-25 16:35:50 +0300

    Adds test cases for Pawn piece.

--- src/test/java/com/kalomiris/pieces/PawnTest.java
+++ src/test/java/com/kalomiris/pieces/PawnTest.java
@@ -7,6 +7,7 @@ import static org.junit.Assert.*;
 
 public class PawnTest {
     private Pawn pawn = new Pawn(0, 1, "White");
+    private Pawn pawn2 = new Pawn(0, 6, "Black");
 
     @Test
     public void isValidPath() throws Exception {
@@ -16,6 +17,13 @@ public class PawnTest {
 
         assertTrue(pawn.isValidPath(0, 2));
         assertTrue(pawn.isValidPath(0, 3));
+
+        assertFalse(pawn2.isValidPath(0, 3));
+        assertFalse(pawn2.isValidPath(1, 4));
+        assertFalse(pawn2.isValidPath(0, 7));
+
+        assertTrue(pawn2.isValidPath(0, 5));
+        assertTrue(pawn2.isValidPath(0, 4));
     }
 
     @Test
@@ -26,6 +34,14 @@ public class PawnTest {
         assertArrayEquals(path, pawn.drawPath(0, 3));
         assertArrayEquals(path1, pawn.drawPath(0, 2));
         assertArrayEquals(path2, pawn.drawPath(1, 2));
+
+
+        int[][] path3 = {{0,6},{0,5},{0,4}};
+        int[][] path4 = {{0,6},{0,5}};
+        int[][] path5 = {{0,6},{1,5}};
+        assertArrayEquals(path3, pawn2.drawPath(0, 4));
+        assertArrayEquals(path4, pawn2.drawPath(0, 5));
+        assertArrayEquals(path5, pawn2.drawPath(1, 5));
     }
 
 }
